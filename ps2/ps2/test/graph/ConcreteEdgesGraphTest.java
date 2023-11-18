8d74c4aaa5fdff765d9cc464f9160dde899addfa
Shumailseecs21 <114047978+Shumailseecs21@users.noreply.github.com>
2023-11-19 01:52:18 +0500

    test cases for graph edges and graph vertices have been added

--- ps2/ps2/test/graph/ConcreteEdgesGraphTest.java
+++ ps2/ps2/test/graph/ConcreteEdgesGraphTest.java
@@ -15,31 +15,82 @@ import org.junit.Test;
  * 
  * Tests against the Graph spec should be in GraphInstanceTest.
  */
+
+
 public class ConcreteEdgesGraphTest extends GraphInstanceTest {
-    
-    /*
-     * Provide a ConcreteEdgesGraph for tests in GraphInstanceTest.
-     */
-    @Override public Graph<String> emptyInstance() {
-        return new ConcreteEdgesGraph();
+
+    @Override
+    public Graph<String> emptyInstance() {
+        return new ConcreteEdgesGraph<>();
     }
-    
-    /*
-     * Testing ConcreteEdgesGraph...
-     */
-    
-    // Testing strategy for ConcreteEdgesGraph.toString()
-    //   TODO
-    
-    // TODO tests for ConcreteEdgesGraph.toString()
-    
-    /*
-     * Testing Edge...
-     */
-    
-    // Testing strategy for Edge
-    //   TODO
-    
-    // TODO tests for operations of Edge
-    
+
+    @Test
+    public void testAddVertex() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("A"));
+        assertTrue(graph.vertices().contains("A"));
+
+        // Adding more vertices
+        assertTrue(graph.add("B"));
+        assertTrue(graph.vertices().contains("B"));
+
+        // Adding duplicate vertex (should not be allowed)
+        assertFalse(graph.add("A"));
+    }
+
+    @Test
+    public void testAddEdge() {
+        Graph<String> graph = emptyInstance();
+        assertEquals(0, graph.set("A", "B", 1));
+        assertTrue(graph.vertices().contains("A"));
+        assertTrue(graph.vertices().contains("B"));
+
+        // Adding multiple edges
+        assertEquals(0, graph.set("B", "C", 2));
+        assertEquals(1, graph.set("A", "B", 3));
+
+        // Updating the weight of an existing edge
+        assertEquals(2, graph.set("B", "C", 4));
+
+        // Adding an edge with zero weight (should remove the edge)
+        assertEquals(4, graph.set("B", "C", 0));
+        assertFalse(graph.targets("B").containsKey("C"));
+        assertFalse(graph.sources("C").containsKey("B"));
+
+        // Adding edges with vertices that do not exist
+        assertEquals(0, graph.set("D", "E", 5));
+        assertTrue(graph.vertices().contains("D"));
+        assertTrue(graph.vertices().contains("E"));
+    }
+
+    @Test
+    public void testRemoveVertex() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        assertTrue(graph.remove("A"));
+        assertFalse(graph.vertices().contains("A"));
+
+        // Removing a vertex with edges connected
+        graph.set("B", "C", 1);
+        assertTrue(graph.remove("B"));
+        assertFalse(graph.vertices().contains("B"));
+        assertFalse(graph.targets("C").containsKey("B"));
+
+        // Removing a non-existing vertex
+        assertFalse(graph.remove("D"));
+    }
+
+
+    @Test
+    public void testStringRepresentation() {
+        Graph<String> graph = emptyInstance();
+        assertEquals("Empty Graph", graph.toString());
+
+        graph.set("A", "B", 1);
+        graph.set("C", "A", 2);
+
+        String expected = "A -> B: 1\nC -> A: 2";
+        assertEquals(expected, graph.toString());
+    }
 }
+
--- ps2/ps2/test/graph/ConcreteVerticesGraphTest.java
+++ ps2/ps2/test/graph/ConcreteVerticesGraphTest.java
@@ -15,31 +15,71 @@ import org.junit.Test;
  * 
  * Tests against the Graph spec should be in GraphInstanceTest.
  */
+
+
+
+
 public class ConcreteVerticesGraphTest extends GraphInstanceTest {
-    
-    /*
-     * Provide a ConcreteVerticesGraph for tests in GraphInstanceTest.
-     */
-    @Override public Graph<String> emptyInstance() {
-        return new ConcreteVerticesGraph();
+
+    @Override
+    public Graph<String> emptyInstance() {
+        return new ConcreteVerticesGraph<>();
     }
-    
-    /*
-     * Testing ConcreteVerticesGraph...
-     */
-    
-    // Testing strategy for ConcreteVerticesGraph.toString()
-    //   TODO
-    
-    // TODO tests for ConcreteVerticesGraph.toString()
-    
-    /*
-     * Testing Vertex...
-     */
-    
-    // Testing strategy for Vertex
-    //   TODO
-    
-    // TODO tests for operations of Vertex
-    
+
+    @Test
+    public void testGraphCreationAndInitialization() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.vertices().isEmpty());
+    }
+
+    @Test
+    public void testAddingVertices() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("A"));
+        assertTrue(graph.vertices().contains("A"));
+
+        assertTrue(graph.add("B"));
+        assertTrue(graph.vertices().contains("B"));
+
+        assertFalse(graph.add("A")); // Adding duplicate vertex
+    }
+
+    @Test
+    public void testAddingEdges() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+
+        assertEquals(0, graph.set("A", "B", 5)); // Add edge from A to B with weight 5
+        assertEquals(5, graph.set("A", "B", 3)); // Update edge weight from 5 to 3
+        assertEquals(3, graph.set("A", "B", 0)); // Remove edge from A to B
+        assertFalse(graph.set("C", "D", 2) > 0); // Add edge with non-existing vertices
+    }
+
+    @Test
+    public void testRemovingVertices() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 2);
+
+        assertTrue(graph.remove("A"));
+        assertFalse(graph.vertices().contains("A"));
+        assertFalse(graph.remove("C")); // Remove non-existing vertex
+    }
+
+    @Test
+    public void testStringRepresentation() {
+        Graph<String> graph = emptyInstance();
+        graph.add("A");
+        graph.add("B");
+        graph.set("A", "B", 2);
+        graph.set("B", "A", 3);
+
+        String expectedString = "A -> {B=2}\nB -> {A=3}";
+        assertEquals(expectedString, graph.toString());
+    }
+
+    // Add more test methods for other scenarios...
+
 }
